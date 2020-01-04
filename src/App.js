cab799ef04359704f6f4ac5c2c958df82d09a3e2
Niroshan-Krishnamoorthy <niroshantrinity@gmail.com>
2020-01-04 21:29:32 +0530

    adding files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Navigation from './components/Navigation/Navigation.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation/>
+      {/* <Logo/>
+      <ImageLinkedForm/>
+      <FaceRecognition/> */}
+    </div>
+  );
+}
+
+export default App;
