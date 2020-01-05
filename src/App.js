7dceb32ff535fe16bd1e1c3331c8ea202b97537b
Niroshan-Krishnamoorthy <niroshantrinity@gmail.com>
2020-01-05 23:19:14 +0530

    face detection api added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Clarifai from 'clarifai';
 import './App.css';
 import Navigation from './components/Navigation/Navigation.js';
 import ImageLinkedForm from './components/ImageLinkedForm/ImageLinkedForm.js';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition.js';
 import Logo from './components/Logo/Logo.js';
 import Rank from './components/Rank/Rank.js';
 import Particles from 'react-particles-js';
@@ -18,19 +20,61 @@ const particleOptions={
   }
 }
 
-function App() {
-  return (
-    <div className="App">
-      <Particles className="particles" params={particleOptions}/>
-      <Navigation/>
-      <Logo/>
-      <Rank/>
-      <ImageLinkedForm/>
-      
-      {/*
-      <FaceRecognition/> */}
-    </div>
+
+const app = new Clarifai.App({
+  apiKey: '5d7adf2b2682472c8feb0dfa9b1b41ee'
+ }); 
+ 
+
+class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state={
+      input:'',
+      imageUrl:''
+    }
+
+  }
+
+  onInputChange = (event)=>{
+    this.setState({input:event.target.value});
+  }
+
+  onButtonSubmit= ()=>{
+
+    this.setState({imageUrl:this.state.input});
+
+    app.models.predict(
+      "a403429f2ddf4b49b307e318f00e528b",
+       this.state.input)
+    .then(
+    function(response) {
+      console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
+    },
+    function(err) { 
+      // there was an error
+    }
   );
+
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Particles className="particles" params={particleOptions}/>
+        <Navigation/>
+        <Logo/>
+        <Rank/>
+        <ImageLinkedForm  
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition imageUrl={this.state.imageUrl}/> 
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/ImageLinkedForm/ImageLinkedForm.js
+++ src/components/ImageLinkedForm/ImageLinkedForm.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './ImageLinkedForm.css';
 
-const ImageLinkedForm= ()=>{
+
+const ImageLinkedForm= ({onInputChange,onButtonSubmit})=>{
     return(
         <div>
             <p className='f3'>{'This is a Face reconition app give it a try!'}</p>
             <div className='center'>
                 <div className='form center pa4 br3 shadow-5'>
-                    <input className='f4 pa2 w-70 center' type='text'/>
-                    <button style={{backgroundColor:'rgb(99, 132, 156)'}} className='w-30 grow f4 link ph3 pv2 dib white'>Detect</button>
+                    <input className='f4 pa2 w-70 center' type='text' 
+                            onChange={onInputChange}
+                    />
+                    <button style={{backgroundColor:'rgb(99, 132, 156)'}} 
+                            className='w-30 grow f4 link ph3 pv2 dib white'
+                            onClick={onButtonSubmit}
+                    >Detect</button>
                 </div>
             </div>
         </div>
