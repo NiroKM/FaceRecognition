401885089014118243223434005027eb82f06aac
Niroshan-Krishnamoorthy <niroshantrinity@gmail.com>
2020-01-22 21:59:35 +0530

    Updates

---
+++ precache-manifest.39c0423ec3c79686a43019c4e7ac9c3a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d32850f0933df9f343c5cf96ba338a04",
+    "url": "/FaceRecognition/index.html"
+  },
+  {
+    "revision": "beee9b070e0850d452e4",
+    "url": "/FaceRecognition/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "6de6c144e5057b819c6f",
+    "url": "/FaceRecognition/static/css/main.fdfe2f93.chunk.css"
+  },
+  {
+    "revision": "beee9b070e0850d452e4",
+    "url": "/FaceRecognition/static/js/2.f730495d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/FaceRecognition/static/js/2.f730495d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6de6c144e5057b819c6f",
+    "url": "/FaceRecognition/static/js/main.a6899592.chunk.js"
+  },
+  {
+    "revision": "c32b0b20fe842a725b6f",
+    "url": "/FaceRecognition/static/js/runtime-main.6dcc92b6.js"
+  }
+]);
